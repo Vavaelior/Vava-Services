f5fbdc8dec63ca7000433aa405571eab9887d8ef
i20h <tibodeplancke667@gmail.com>
2026-01-31 22:45:53 +0100

    feat(VavaRadar): initialize ESX shared object in sh_main.lua

--- sh_main.lua
+++ sh_main.lua
@@ -1,4 +1,5 @@
 ---Les functions suivantes peuvent être utilisées à la fois côté client et côté serveur à vous de voir si vous voulez optimiser le reseau ou pas (actuellement pas le cas)
+ESX = exports['es_extended']:getSharedObject();
 
 ---@param speed number
 ---@param speedLimit number
