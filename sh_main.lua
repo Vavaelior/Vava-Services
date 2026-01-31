28e81773cf801bb8eab14d20b647cff5570f42e9
i20h <tibodeplancke667@gmail.com>
2026-01-31 22:32:50 +0100

    feat(VavaRadar): add functions to calculate fines and convert speed to km/h

---
+++ sh_main.lua
@@ -0,0 +1,15 @@
+---Les functions suivantes peuvent être utilisées à la fois côté client et côté serveur à vous de voir si vous voulez optimiser le reseau ou pas (actuellement pas le cas)
+
+---@param speed number
+---@param speedLimit number
+---@return number
+function calculateFine(speed, speedLimit)
+    local speedOverLimit = speed - speedLimit
+    return speedOverLimit > 0 and math.floor(speedOverLimit * config.finePerKmh) or 0;
+end
+
+---@param speed number
+---@return number
+function msToKmh(speed)
+    return speed * 3.6;
+end
